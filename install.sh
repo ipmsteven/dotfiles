7c071c57f68d2dc47f548579c3973bbe05601f57
Yunlei Liu <ipmsteven@github.com>
2020-05-05 22:00:31 -0700

    oh_my_zsh

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@
 # Install Vim
 curl -L https://bit.ly/janus-bootstrap | bash
 
+# Install zsh and ohmyzsh
+if [ "$SHELL" != "/usr/bin/zsh" ]; then
+  sudo apt install -y zsh
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi;
 
 # link dotfiles
 dotdir=$(cd $(dirname $0) && pwd)
