8ec907df1e3c31c5752e49ec9ebc0d12be743ee2
Yunlei Liu <ipmsteven@github.com>
2020-05-05 22:24:29 -0700

    re install tmux 3 on bp dev box

--- install.sh
+++ install.sh
@@ -10,7 +10,28 @@ if [ "$SHELL" != "/usr/bin/zsh" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi;
 
-# link dotfiles
+# Reinstall tmux 3 on bp-dev box
+
+sudo apt remove -y tmux
+
+sudo apt install -y git
+sudo apt install -y automake
+sudo apt install -y build-essential
+sudo apt install -y pkg-config
+sudo apt install -y libevent-dev
+sudo apt install -y libncurses5-dev
+sudo apt install -y bison
+
+rm -fr /tmp/tmux
+git clone https://github.com/tmux/tmux.git /tmp/tmux
+cd /tmp/tmux
+sh autogen.sh
+./configure && make
+sudo make install
+cd -
+rm -fr /tmp/tmux
+
+# Link dotfiles
 dotdir=$(cd $(dirname $0) && pwd)
 for filename in $(find $dotdir -name "*.symlink"); do
   ln -sf $filename ~/.$(basename $filename | sed "s/\.symlink//")
